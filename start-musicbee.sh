8c6c0efbda7758df19c99d7775124bad84320395
COLVDV <122833328+colvdv@users.noreply.github.com>
2026-03-15 13:01:11 -0900

    Cleaned up launch script
    Users need only ensure that the WINEPREFIX is pointing to the correct location for their setup now.

--- start-musicbee.sh
+++ start-musicbee.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # MusicBee + MPRIS Launch Script
 
-#If needed, update this to point to the appropriate wine prefix.
 export WINEPREFIX="$HOME/.wine"
 
 # 1. Start the Linux Bridge
-## Replace USERNAME with your username
-"/home/USERNAME/.wine/drive_c/Program Files (x86)/MusicBee/mprisbee-bridge" &
+"$WINEPREFIX/drive_c/Program Files (x86)/MusicBee/mprisbee-bridge" &
 
 # 2. Wait 1 second for the bridge to initialize
 sleep 1
